c8fbf6e58ede54a428cf003990322d1447d257ee
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2021-02-07 20:57:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution here!
+const drivers = ["Milo",'Otis','Garfield']
+
+function destructivelyAppendDriver(name) {
+  
+}
+
+function destructivelyPrependDriver(name) {
+  
+}
+
+function destructivelyRemoveLastDriver() {
+ 
+}
+
+function appendDriver(name) {
+  
+}
+
+function destructivelyRemoveFirstDriver() { 
+ 
+}
+
+
+function appendDriver (name) {
+  
+}
+
+function prependDriver (name) {
+ 
+}
+
+function removeFirstDriver () {
+  
+}
+
+function removeLastDriver () {
+  
+}
